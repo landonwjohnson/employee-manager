dc8dfba4d85f0b357ed2b89cf2492107efd43934
landonwjohnson <landonwjohnson@gmail.com>
2017-09-13 07:53:58 -0600

    on stage 9

--- src/Stage 9/App.js
+++ src/Stage 9/App.js
@@ -14,8 +14,8 @@ class App extends Component {
     this.state = {
       employees: [ new Employee(0, 'Cahlan Sharp', 4824931093, 'Co-Founder, CEO', 'Provo, Utah'), new Employee(1, 'Tyler Richards', 3094812387, 'Co-Founder, VP', 'Provo, Utah'), new Employee(2, 'Mark Hanson', 7459831843, 'VP of Ops', 'Provo, Utah'), new Employee(3, 'Meaghan Barber', 3841238745, 'Employer Relations', 'Provo, Utah'), new Employee(4, 'Niall Cummins', 4873459812, 'Software Developer', 'Provo, Utah'), new Employee(5, 'Cameron Kinney', 6571249801, 'Marketing Manager', 'Provo, Utah'), new Employee(6, 'Brandon Hassler', 5740923478, 'Director of Marketing', 'Provo, Utah'), new Employee(7, 'Brett Shaffer', 8727813498, 'Director of Finance', 'Provo, Utah'), new Employee(8, 'Cory Johnson', 8734567810, 'Curriculum Designer', 'Provo, Utah'), new Employee(9, 'James Lemire', 8749823456, 'Curriculum Developer', 'Provo, Utah') ],
       selectedEmployee: null
-    }
-    this.selectedEmployee = this.selectEmployee.bind( this );
+    };
+    this.selectEmployee = this.selectEmployee.bind( this );
     this.refresh = this.refresh.bind( this );
   }
   // selectEmployee
@@ -29,10 +29,10 @@ class App extends Component {
   render() {
     return (
       <div id="app">
-        // render Header here
+          <Header />
         <div className="main-container">
-          // render EmployeeList here
-          // render EmployeeEditor
+          <EmployeeList employees={this.state.employees} selectEmployee={ this.selectEmployee } />
+          <EmployeeEditor selected={this.state.selectedEmployee} refreshList={ this.refresh } />
         </div>
       </div>
     )
--- src/Stage 9/components/EmployeeEditor.js
+++ src/Stage 9/components/EmployeeEditor.js
@@ -13,15 +13,33 @@ class EmployeeEditor extends Component {
     this.cancel = this.cancel.bind( this );
   }
   // componentWillReceiveProps
-  componentWillRecieveProps(props) {
-    this.setState({employee: Object.assign({})})
+  componentWillReceiveProps(props) {
+    this.setState({employee: Object.assign({}, props.selected), originalEmployee: props.selected, notModified: true});
   }
 
   // handleChange
-
+  handleChange(prop, val) {
+    if( this.state.notModified ) {
+      this.setState({ notModified: false })
+    }
+    var employeeCopy = Object.assign({}, this.state.employee);
+    employeeCopy[prop] = val;
+    this.setState({ employee: employeeCopy})
+  }
   // save
-
+  save() {
+    this.state.originalEmployee.updateName(this.state.employee.name);
+    this.state.originalEmployee.updatePhone(this.state.employee.phone);
+    this.state.originalEmployee.updateTitle(this.state.employee.title);
+    this.state.originalEmployee.updateLocation(this.state.employee.location);
+    this.setState({ notModified: true });
+    this.props.refreshList();
+  }
   // cancel
+  cancel() {
+    var employeeCopy = Object.assign({}, this.state.employee.name);
+    this.setState({employee: employeeCopy, notModified: true});
+  }
   
   render() {
     return (
@@ -42,6 +60,8 @@ class EmployeeEditor extends Component {
             <input className="materialInput" value={ this.state.employee.phone } onChange={ (e) => { this.handleChange('phone', e.target.value) } }></input>
             <span className="placeholderText"> Title </span>
             <input className="materialInput" value={ this.state.employee.title } onChange={ (e) => { this.handleChange('title', e.target.value) } }></input>
+            <span className="placeholderText"> Office Location </span>
+            <input className="materialInput" value={ this.save.employee.location } onChange={ (e) => { this.handleChange('location', e.target.value) } }> </input>
           </div>
           :
           <p id="noEmployee"> No Employee Selected </p>
